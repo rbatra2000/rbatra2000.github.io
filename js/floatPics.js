8b1750a3b2e3effddd55adb260d1f17af85fdb2a
Ritik Batra <ritik.batra@rallyhealth.com>
2019-08-03 12:54:22 -0700

    transferring to other computer

---
+++ js/floatPics.js
@@ -0,0 +1,15 @@
+function move() {
+    var elem = document.getElementById("about");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
