bfea18c68b586db011d5e5e40559b6bd6157df03
rbatra2000 <ritik.batra2000@gmail.com>
2019-08-04 10:48:01 -0700

    Made some changes for adding animation

---
+++ js/name.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('name'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets4.lottiefiles.com/packages/lf20_8DJAlf.json'
+});
