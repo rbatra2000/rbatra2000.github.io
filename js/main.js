1f8aecace11bb470d7342f6e676f4fb3930635fb
rbatra2000 <ritik.batra2000@gmail.com>
2019-08-16 17:27:08 -0700

    Worked on Nav bar

---
+++ js/main.js
@@ -0,0 +1,59 @@
+document.body.className = 'fade';
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+    })
+});
+
+$(window).on("scroll", function() {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    } else {
+        $('nav').removeClass('black')
+    }
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        $("nav ul").toggleClass("showing");
+    }
+
+});
+
+var animateHTML = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+        elems = document.getElementsByClassName('hidden');
+        windowHeight = window.innerHeight;
+        addEventHandlers();
+        checkPosition();
+    }
+    function addEventHandlers() {
+        window.addEventListener('scroll', checkPosition);
+        window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+        for (var i = 0; i < elems.length; i++) {
+            var positionFromTop = elems[i].getBoundingClientRect().top;
+            if (positionFromTop - windowHeight <= 0) {
+                elems[i].className = elems[i].className.replace(
+                    'hidden',
+                    'fade-in-element'
+                );
+            }
+        }
+    }
+    return {
+        init: init
+    };
+};
+animateHTML().init();
