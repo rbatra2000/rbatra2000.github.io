27daaef6832bad3d903706d9e2ac39e2d2d32b48
rbatra2000 <ritik.batra2000@gmail.com>
2020-05-17 16:14:22 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Content } from 'react-mdl';
+import { Navbar } from 'react-bootstrap';
 import Main from './components/main';
 import './App.css';
+import logo from './assets/logo.png';
+
 
 function App() {
   return (
-      <div className="demo-big-content">
-        <Layout>
-            <Header className="header-color" title="Title" scroll>
-                <Navigation>
-                    {/* <Link to="/resume">Resume</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link> */}
-                </Navigation>
-            </Header>
-            <Drawer title="Title">
-                <Navigation>
-                    {/* <Link to="/resume">Resume</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link> */}
-                </Navigation>
-            </Drawer>
-            <Content>
-              <div className="page-content" />
-              <Main /> 
-            </Content>
-        </Layout>
-      </div>    
+    <div className="navbar">
+      <Layout>
+        <Navbar>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Navbar>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
   );
 }
 
