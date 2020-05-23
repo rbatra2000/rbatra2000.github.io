a17bf5b08b675a69137aeb6d64eaa67d959b7ecf
rbatra2000 <ritik.batra2000@gmail.com>
2020-05-23 00:35:31 -0700

    Old Website addition

---
+++ OldWebsite/Website3/js/particle.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js', 'https://raw.githubusercontent.com/rbatra2000/rbatra2000.github.io/master/assets/homeParticles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
