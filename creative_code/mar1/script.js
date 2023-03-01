69258e4d8a7ec0cb3b407d38bea0bde31076f1a2
Ritik Batra <ritikbatra@berkeley.edu>
2023-03-01 18:09:04 -0500

    added creative code

---
+++ creative_code/mar1/script.js
@@ -0,0 +1,32 @@
+const minSize = 1
+const maxSize = 100
+const stepSize = 5;
+const lineWeight = 1.5;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noLoop();
+}
+
+function draw() {
+  background(200);
+  push();
+  for (let i = 0; i < width; i += maxSize) {
+    for (let j = 0; j < height; j += maxSize)
+    makeSquare(i, j, maxSize);
+  }
+  pop();
+}
+
+function makeSquare(x, y, length) {
+  if (length < minSize) {
+    return;
+  }
+  const xOffset = Math.random() * stepSize/2 - stepSize;
+  const yOffset = Math.random() * stepSize/2 - stepSize/2;
+  noFill();
+  strokeWeight(lineWeight);
+  rect(x+xOffset,y+yOffset, length, length);
+  const newLength = length-stepSize;
+  makeSquare(x+stepSize/2,y+stepSize/2,newLength)
+}
