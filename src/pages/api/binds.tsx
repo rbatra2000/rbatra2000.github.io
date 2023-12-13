6cf8a2a2a8f26b4ff2dd68dddcbba086b8862f98
Ritik Batra <ritikbatra@berkeley.edu>
2023-12-13 00:11:19 -0500

    woot

---
+++ src/pages/api/binds.tsx
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+var sharedTxt = "hi b, i love you"
+var dateObj = new Date();
+ 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Process a POST request
+    const { msg } = req.query
+    sharedTxt = (msg as string)
+    res.status(200).json({ msg: "MESSAGE SENT!!" });
+  } else {
+    // Handle any other HTTP method
+    var time = dateObj.toLocaleTimeString([], {weekday: 'short', hour: '2-digit', minute: '2-digit'})
+    res.status(200).json({ msg: sharedTxt, time });
+  }
+}
