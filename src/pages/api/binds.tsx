45d1eeff949147259102194838717a2270553dd7
Ritik Batra <ritikbatra@berkeley.edu>
2023-12-17 19:21:34 -0500

    trying this for CORS policy

--- src/pages/api/binds.tsx
+++ src/pages/api/binds.tsx
@@ -3,11 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 var sharedTxt = "hi b, i love you"
  
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  
   if (req.method === 'POST') {
     // Process a POST request
     const { msg } = req.query
     sharedTxt = (msg as string)
     res.status(200).json({ msg: "MESSAGE SENT!!" });
+    
   } else {
     // Handle any other HTTP method
     var dateObj = new Date();
