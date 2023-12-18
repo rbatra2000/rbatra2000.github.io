bf5d01a14bfbea16be06d94674da721c3011e835
Ritik Batra <ritikbatra@berkeley.edu>
2023-12-17 19:36:37 -0500

    Try again

--- src/pages/api/binds.tsx
+++ src/pages/api/binds.tsx
@@ -3,13 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 var sharedTxt = "hi b, i love you"
  
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', "true")
+  res.setHeader('Access-Control-Allow-Origin', '*')
   
   if (req.method === 'POST') {
     // Process a POST request
     const { msg } = req.query
     sharedTxt = (msg as string)
-    res.status(200).json({ msg: "MESSAGE SENT!!" });
-    
+    res.json({ msg: "MESSAGE SENT!!" });
   } else {
     // Handle any other HTTP method
     var dateObj = new Date();
